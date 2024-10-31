01cc2fe21f06755a68c578128f04fcf9a7ab6afb
Ariel Lázaro <ariellazaro444@gmail.com>
2024-10-31 19:14:41 -0400

    Configure automapper profile

--- LazaBarbershop.Core.Application/Mappings/GeneralProfile.cs
+++ LazaBarbershop.Core.Application/Mappings/GeneralProfile.cs
@@ -1,4 +1,8 @@
 using AutoMapper;
+using LazaBarbershop.Core.Application.Dtos.Appointments;
+using LazaBarbershop.Core.Application.Dtos.Barbershops;
+using LazaBarbershop.Core.Application.Dtos.Schedules;
+using LazaBarbershop.Core.Domain.Entities;
 
 namespace LazaBarbershop.Core.Application.Mappings;
 
@@ -6,6 +10,16 @@ public class GeneralProfile : Profile
 {
     public GeneralProfile()
     {
-        
+        // APPOINTMENT MAPPINGS
+        CreateMap<Appointment, AppointmentDto>().ReverseMap();
+        CreateMap<Appointment, SaveAppointmentDto>().ReverseMap();
+
+        // BARBERSHOP MAPPINGS
+        CreateMap<Barbershop, BarbershopDto>().ReverseMap();
+        CreateMap<Barbershop, SaveBarbershopDto>().ReverseMap();
+
+        // SCHEDULE MAPPINGS
+        CreateMap<Schedule, ScheduleDto>().ReverseMap();
+        CreateMap<Schedule, SaveScheduleDto>().ReverseMap();
     }
 }
